b9e1e583cefa5132af6490f6c494be124c8ad9f4
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-19 09:05:34 -0300

    tirei os links

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,6 @@ export default function Home() {
   return (
     <>
       <h1>HOME PAGE</h1>
-
-      <div>
-        <Link href="/produtos/tenis">TÊNIS</Link> |
-        <Link href="/produtos/produto/tenis-preto">TÊNIS PRETO</Link> | 
-        <Link href="/produtos/produto/camisa-xadrez">CAMISA X</Link>
-      </div>
     </>
   )
 }
