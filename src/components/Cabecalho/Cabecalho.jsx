1f1333d049e4375e49fc4445e458b17f92e64e11
rafaellabastos1 <rm552425@fiap.com.br>
2023-10-20 08:22:38 -0300

    cabecalho

--- src/components/Cabecalho/Cabecalho.jsx
+++ src/components/Cabecalho/Cabecalho.jsx
@@ -4,9 +4,9 @@ export default function Cabecalho() {
   return (
         <header className="bg-gray-800 text-white h-16">
           <nav className="flex flex-row justify-around">
-            <Link className="block xl:my-5 sm:my-2 md:my-5" href="/produtos/estaticas/tenis">TÊNIS</Link>
-            <Link className="block xl:my-5 sm:my-2 md:my-5" href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
-            <Link className="block xl:my-5 sm:my-2 md:my-5" href="/produtos/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
+            <Link className="" href="/produtos/estaticas/tenis">TÊNIS</Link>
+            <Link className="" href="/produtos/dinamicas/estranho/tenis-preto">TÊNIS PRETO</Link>
+            <Link className="" href="/produtos/dinamicas/vestuario/camisa-xadrez">CAMISA-X</Link>
           </nav>
         </header>
   )
